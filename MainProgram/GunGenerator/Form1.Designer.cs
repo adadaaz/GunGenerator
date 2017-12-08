124486890ddbbfa5dc153fad1a42f42f6c6064f7
SherlockShiYL <shiyileish@gmail.com>
2017-12-07 21:09:08 -0800

    GunGen

--- MainProgram/GunGenerator/Form1.Designer.cs
+++ MainProgram/GunGenerator/Form1.Designer.cs
@@ -44,20 +44,23 @@
             this.txtMagazine = new System.Windows.Forms.TextBox();
             this.btnModify = new System.Windows.Forms.Button();
             this.comboxManufacturer = new System.Windows.Forms.ComboBox();
+            this.lblManufacturer = new System.Windows.Forms.Label();
+            this.lblWeapon = new System.Windows.Forms.Label();
+            this.lblName = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // btnGenerate
             // 
-            this.btnGenerate.Location = new System.Drawing.Point(44, 293);
+            this.btnGenerate.Location = new System.Drawing.Point(34, 363);
             this.btnGenerate.Name = "btnGenerate";
-            this.btnGenerate.Size = new System.Drawing.Size(209, 23);
+            this.btnGenerate.Size = new System.Drawing.Size(207, 23);
             this.btnGenerate.TabIndex = 0;
             this.btnGenerate.Text = "Generate";
             this.btnGenerate.UseVisualStyleBackColor = true;
             // 
             // btnExport
             // 
-            this.btnExport.Location = new System.Drawing.Point(112, 331);
+            this.btnExport.Location = new System.Drawing.Point(100, 401);
             this.btnExport.Name = "btnExport";
             this.btnExport.Size = new System.Drawing.Size(75, 23);
             this.btnExport.TabIndex = 1;
@@ -66,23 +69,30 @@
             // 
             // cmboxGunType
             // 
+            this.cmboxGunType.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.cmboxGunType.FormattingEnabled = true;
-            this.cmboxGunType.Location = new System.Drawing.Point(45, 69);
+            this.cmboxGunType.Items.AddRange(new object[] {
+            "Pistols",
+            "Submachine Guns",
+            "Shotguns",
+            "Assault Rifles",
+            "Sniper Rifles"});
+            this.cmboxGunType.Location = new System.Drawing.Point(34, 138);
             this.cmboxGunType.Name = "cmboxGunType";
-            this.cmboxGunType.Size = new System.Drawing.Size(209, 21);
+            this.cmboxGunType.Size = new System.Drawing.Size(207, 21);
             this.cmboxGunType.TabIndex = 2;
             // 
             // txtGunName
             // 
-            this.txtGunName.Location = new System.Drawing.Point(46, 12);
+            this.txtGunName.Location = new System.Drawing.Point(34, 41);
             this.txtGunName.Name = "txtGunName";
-            this.txtGunName.Size = new System.Drawing.Size(209, 20);
+            this.txtGunName.Size = new System.Drawing.Size(207, 20);
             this.txtGunName.TabIndex = 3;
             // 
             // lblDamage
             // 
             this.lblDamage.AutoSize = true;
-            this.lblDamage.Location = new System.Drawing.Point(43, 112);
+            this.lblDamage.Location = new System.Drawing.Point(32, 182);
             this.lblDamage.Name = "lblDamage";
             this.lblDamage.Size = new System.Drawing.Size(47, 13);
             this.lblDamage.TabIndex = 4;
@@ -91,7 +101,7 @@
             // lblAccuracy
             // 
             this.lblAccuracy.AutoSize = true;
-            this.lblAccuracy.Location = new System.Drawing.Point(43, 138);
+            this.lblAccuracy.Location = new System.Drawing.Point(32, 208);
             this.lblAccuracy.Name = "lblAccuracy";
             this.lblAccuracy.Size = new System.Drawing.Size(52, 13);
             this.lblAccuracy.TabIndex = 5;
@@ -100,7 +110,7 @@
             // lblFireRate
             // 
             this.lblFireRate.AutoSize = true;
-            this.lblFireRate.Location = new System.Drawing.Point(43, 164);
+            this.lblFireRate.Location = new System.Drawing.Point(32, 234);
             this.lblFireRate.Name = "lblFireRate";
             this.lblFireRate.Size = new System.Drawing.Size(50, 13);
             this.lblFireRate.TabIndex = 6;
@@ -109,7 +119,7 @@
             // lblReloadSpeed
             // 
             this.lblReloadSpeed.AutoSize = true;
-            this.lblReloadSpeed.Location = new System.Drawing.Point(42, 190);
+            this.lblReloadSpeed.Location = new System.Drawing.Point(32, 260);
             this.lblReloadSpeed.Name = "lblReloadSpeed";
             this.lblReloadSpeed.Size = new System.Drawing.Size(75, 13);
             this.lblReloadSpeed.TabIndex = 7;
@@ -118,7 +128,7 @@
             // lblMagazine
             // 
             this.lblMagazine.AutoSize = true;
-            this.lblMagazine.Location = new System.Drawing.Point(43, 216);
+            this.lblMagazine.Location = new System.Drawing.Point(32, 286);
             this.lblMagazine.Name = "lblMagazine";
             this.lblMagazine.Size = new System.Drawing.Size(76, 13);
             this.lblMagazine.TabIndex = 8;
@@ -126,7 +136,7 @@
             // 
             // txtDamage
             // 
-            this.txtDamage.Location = new System.Drawing.Point(162, 105);
+            this.txtDamage.Location = new System.Drawing.Point(150, 175);
             this.txtDamage.Name = "txtDamage";
             this.txtDamage.ReadOnly = true;
             this.txtDamage.Size = new System.Drawing.Size(91, 20);
@@ -134,7 +144,7 @@
             // 
             // txtAccuracy
             // 
-            this.txtAccuracy.Location = new System.Drawing.Point(162, 131);
+            this.txtAccuracy.Location = new System.Drawing.Point(150, 201);
             this.txtAccuracy.Name = "txtAccuracy";
             this.txtAccuracy.ReadOnly = true;
             this.txtAccuracy.Size = new System.Drawing.Size(91, 20);
@@ -142,7 +152,7 @@
             // 
             // txtFireRate
             // 
-            this.txtFireRate.Location = new System.Drawing.Point(162, 157);
+            this.txtFireRate.Location = new System.Drawing.Point(150, 227);
             this.txtFireRate.Name = "txtFireRate";
             this.txtFireRate.ReadOnly = true;
             this.txtFireRate.Size = new System.Drawing.Size(91, 20);
@@ -150,7 +160,7 @@
             // 
             // txtReload
             // 
-            this.txtReload.Location = new System.Drawing.Point(162, 183);
+            this.txtReload.Location = new System.Drawing.Point(150, 253);
             this.txtReload.Name = "txtReload";
             this.txtReload.ReadOnly = true;
             this.txtReload.Size = new System.Drawing.Size(91, 20);
@@ -158,7 +168,7 @@
             // 
             // txtMagazine
             // 
-            this.txtMagazine.Location = new System.Drawing.Point(162, 209);
+            this.txtMagazine.Location = new System.Drawing.Point(150, 279);
             this.txtMagazine.Name = "txtMagazine";
             this.txtMagazine.ReadOnly = true;
             this.txtMagazine.Size = new System.Drawing.Size(91, 20);
@@ -166,7 +176,7 @@
             // 
             // btnModify
             // 
-            this.btnModify.Location = new System.Drawing.Point(162, 252);
+            this.btnModify.Location = new System.Drawing.Point(150, 322);
             this.btnModify.Name = "btnModify";
             this.btnModify.Size = new System.Drawing.Size(91, 23);
             this.btnModify.TabIndex = 14;
@@ -176,17 +186,54 @@
             // 
             // comboxManufacturer
             // 
+            this.comboxManufacturer.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.comboxManufacturer.FormattingEnabled = true;
-            this.comboxManufacturer.Location = new System.Drawing.Point(46, 42);
+            this.comboxManufacturer.Items.AddRange(new object[] {
+            "JAKOBS",
+            "TEDIORE",
+            "MALIWAN",
+            "DAHL",
+            "HYPERION"});
+            this.comboxManufacturer.Location = new System.Drawing.Point(34, 91);
             this.comboxManufacturer.Name = "comboxManufacturer";
-            this.comboxManufacturer.Size = new System.Drawing.Size(209, 21);
+            this.comboxManufacturer.Size = new System.Drawing.Size(207, 21);
             this.comboxManufacturer.TabIndex = 15;
+            // 
+            // lblManufacturer
+            // 
+            this.lblManufacturer.AutoSize = true;
+            this.lblManufacturer.Location = new System.Drawing.Point(32, 70);
+            this.lblManufacturer.Name = "lblManufacturer";
+            this.lblManufacturer.Size = new System.Drawing.Size(73, 13);
+            this.lblManufacturer.TabIndex = 16;
+            this.lblManufacturer.Text = "Manufacturer:";
+            // 
+            // lblWeapon
+            // 
+            this.lblWeapon.AutoSize = true;
+            this.lblWeapon.Location = new System.Drawing.Point(32, 119);
+            this.lblWeapon.Name = "lblWeapon";
+            this.lblWeapon.Size = new System.Drawing.Size(51, 13);
+            this.lblWeapon.TabIndex = 17;
+            this.lblWeapon.Text = "Weapon:";
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Location = new System.Drawing.Point(32, 20);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(38, 13);
+            this.lblName.TabIndex = 18;
+            this.lblName.Text = "Name:";
             // 
             // frmGunGenerator
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(680, 415);
+            this.ClientSize = new System.Drawing.Size(680, 446);
+            this.Controls.Add(this.lblName);
+            this.Controls.Add(this.lblWeapon);
+            this.Controls.Add(this.lblManufacturer);
             this.Controls.Add(this.comboxManufacturer);
             this.Controls.Add(this.btnModify);
             this.Controls.Add(this.txtMagazine);
@@ -228,6 +275,9 @@
         private System.Windows.Forms.TextBox txtMagazine;
         private System.Windows.Forms.Button btnModify;
         private System.Windows.Forms.ComboBox comboxManufacturer;
+        private System.Windows.Forms.Label lblManufacturer;
+        private System.Windows.Forms.Label lblWeapon;
+        private System.Windows.Forms.Label lblName;
     }
 }
 
